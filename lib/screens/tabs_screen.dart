949b9cd7a765819a74ca9fdb09ab7737570dce48
Sant'Clear <santclear@gmail.com>
2023-03-10 09:53:57 -0300

    18 - Refactored TabsScreen widget to use BottomNavigationBar for improved tab bar functionality, replacing DefaultTabController. Updated tab labels and icons for a more user-friendly experience.

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,38 +2,61 @@ import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_screen.dart';
 import 'package:meals/screens/favorite_screen.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
 
   @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedScreenIndex = 0;
+
+  final List<Map<String, Object>> _screens = [
+    {
+      'title': 'Categories List',
+      'screen': const CategoriesScreen(),
+    },
+    {
+      'title': 'My Favorites',
+      'screen': const FavoriteScreen(),
+    },
+  ];
+
+  _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Center(
-            child: Text('Let\'s cook?'),
-          ),
-          bottom: const TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favorites',
-              ),
-            ],
-          ),
-        ),
-        body: const TabBarView(
-          children: [
-            CategoriesScreen(),
-            FavoriteScreen(),
-          ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(_screens[_selectedScreenIndex]['title'] as String),
         ),
       ),
+      body: _screens[_selectedScreenIndex]['screen'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectScreen,
+          backgroundColor: Theme.of(context).primaryColor,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Theme.of(context).colorScheme.secondary,
+          currentIndex: _selectedScreenIndex,
+          // type: BottomNavigationBarType.shifting,
+          items: const [
+            BottomNavigationBarItem(
+              // backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.category),
+              label: 'Categories',
+            ),
+            BottomNavigationBarItem(
+              // backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.star),
+              label: 'Favorites',
+            ),
+          ]),
     );
   }
 }
